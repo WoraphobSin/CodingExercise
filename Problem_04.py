3621c88ec9a333d7651d5cfb916286ab825a2f61
Woraphob <Woraphob2349@gmail.com>
2024-07-12 10:44:01 +0700

    Change name
    Change name correspond to code

--- Problem_04.py
+++
@@ -1,3 +0,0 @@
-a = int(input())
-b = int(input())
-print(f"{int(a/b)} km/h")
--- Sorting number.py
+++ Sorting number.py
@@ -4,5 +4,6 @@ for y in range(0,j):
     z = int(input())
     x.append(z)
 x.sort()
+print(x)
 for i in range(0,j):
-    print(x[j-1-i])
+    print(x[j-i-1])
