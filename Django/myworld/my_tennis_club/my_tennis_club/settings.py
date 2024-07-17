197492b42051269edaee6d58a0c7621e202ab770
Woraphob <Woraphob2349@gmail.com>
2024-07-17 09:33:24 +0700

    update
    connect to aws database and prep for deploy

--- Django/myworld/my_tennis_club/my_tennis_club/settings.py
+++ Django/myworld/my_tennis_club/my_tennis_club/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER':'masteruser',
+        'PASSWORD': 'Destroyer_0x02',
+        'HOST':'w3-django-project.cvyeqea6aweg.ap-southeast-2.rds.amazonaws.com',
+        'PORT':'5432'
+    }   
 }
 
 
