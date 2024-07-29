ae0baf81a980c78fb820f97849ee29be2f5dd910
Woraphob <Woraphob2349@gmail.com>
2024-07-29 09:19:16 +0700

    Create Two Sum.py

---
+++ Two Sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []  # No solution found
+
+test = Solution()
+nums = [2,6,1,7,3,9]
+target = 12
+print(test.twoSum(nums,target))
